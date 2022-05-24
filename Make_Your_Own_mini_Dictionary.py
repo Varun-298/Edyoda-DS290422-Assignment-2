f8c7341f8d688d46eed58e55e2c812260a97da45
Varun Rohit Vadapalli <105191986+Varun-298@users.noreply.github.com>
2022-05-24 16:22:27 +0530

    Update Make_Your_Own_mini_Dictionary.py

--- Make_Your_Own_mini_Dictionary.py
+++ Make_Your_Own_mini_Dictionary.py
@@ -2,12 +2,12 @@
 # Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
 
 dictionary = {} 
-keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+keys = []
 values=[]
 
-for i in range(97,97+len(keys)):
-    val = i
-    values.append(val)
+for i in range(97,123):
+    keys.append(chr(i))
+    values.append(i)
 
 dictionary = dict(zip(keys,values))
 print("Dictionay with Alphabetical keys and corresponding ASCII values :",dictionary)
